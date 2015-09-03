302a0757b931c5a0db619bf4fbd063a51802fde3
srenner <shawn.renner@gmail.com>
2015-09-02 21:29:09 -0500

    setup rest framework

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from mainsite.serializers import UserSerializer
+from rest_framework import viewsets
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
--- photerva/urls.py
+++ photerva/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from mainsite import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
