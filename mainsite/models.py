26056cae5e662b38962ab4e60eca8714eb69b36d
srenner <shawn.renner@gmail.com>
2015-09-08 21:41:43 -0500

    code style

--- mainsite/models.py
+++ mainsite/models.py
@@ -5,12 +5,12 @@ from django.db.models.fields.related import ForeignKey
 #########PARENT CLASSES WE ARE MOST INTERESTED IN##########
 
 class Location(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
     notes = models.TextField()
     user = models.ForeignKey(User)
 
 class Customer(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
     notes = models.TextField()
     user = models.ForeignKey(User)
 
@@ -18,9 +18,9 @@ class Customer(models.Model):
         return self.name
 
 class SessionType(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
     notes = models.TextField()
-    base_price = models.DecimalField(max_digits = 8, decimal_places = 2)
+    base_price = models.DecimalField(max_digits=8, decimal_places=2)
     shoot_time = models.TimeField()
     edit_time = models.TimeField()
     user = models.ForeignKey(User)
@@ -29,9 +29,9 @@ class Session(models.Model):
     datetime = models.DateTimeField()
     backup_datetime = models.DateTimeField()
     notes = models.TextField()
-    quoted_price = models.DecimalField(max_digits = 8, decimal_places = 2)
-    final_price = models.DecimalField(max_digits = 8, decimal_places = 2)
-    expenses = models.DecimalField(max_digits = 8, decimal_places = 2)
+    quoted_price = models.DecimalField(max_digits=8, decimal_places=2)
+    final_price = models.DecimalField(max_digits=8, decimal_places=2)
+    expenses = models.DecimalField(max_digits=8, decimal_places=2)
     shoot_time = models.TimeField()
     edit_time = models.TimeField()
     user = models.ForeignKey(User)
@@ -39,32 +39,32 @@ class Session(models.Model):
 ##########CHILD CLASSES##########
 
 class Phone(models.Model):
-    name = models.CharField(max_length = 100)
-    phone = models.CharField(max_length = 25)
-    ext = models.CharField(max_length = 25, blank=True, null=True)
-    location = models.ForeignKey('Location', blank = True, null = True)
-    customer = models.ForeignKey('Customer', blank = True, null = True)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=25)
+    ext = models.CharField(max_length=25, blank=True, null=True)
+    location = models.ForeignKey('Location', blank=True, null=True)
+    customer = models.ForeignKey('Customer', blank=True, null=True)
 
 class Email(models.Model):
-    name = models.CharField(max_length = 100)
-    email = models.CharField(max_length = 200)
-    location = models.ForeignKey('Location', blank = True, null = True)
-    customer = models.ForeignKey('Customer', blank = True, null = True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    location = models.ForeignKey('Location', blank=True, null=True)
+    customer = models.ForeignKey('Customer', blank=True, null=True)
 
 class Link(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
     url = models.URLField()
-    location = models.ForeignKey('Location', blank = True, null = True)
-    customer = models.ForeignKey('Customer', blank = True, null = True)
-    session = models.ForeignKey('Session', blank = True, null = True)
+    location = models.ForeignKey('Location', blank=True, null=True)
+    customer = models.ForeignKey('Customer', blank=True, null=True)
+    session = models.ForeignKey('Session', blank=True, null=True)
 
 class Address(models.Model):
-    name = models.CharField(max_length = 100)
-    address1 = models.CharField(max_length = 100)
-    address2 = models.CharField(max_length = 100)
-    city = models.CharField(max_length = 100)
-    state = models.CharField(max_length = 100)
-    zip = models.CharField(max_length = 100)
-    location = models.ForeignKey('Location', blank = True, null = True)
-    customer = models.ForeignKey('Customer', blank = True, null = True)
-    session = models.ForeignKey('Session', blank = True, null = True)
+    name = models.CharField(max_length=100)
+    address1 = models.CharField(max_length=100)
+    address2 = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip = models.CharField(max_length=100)
+    location = models.ForeignKey('Location', blank=True, null=True)
+    customer = models.ForeignKey('Customer', blank=True, null=True)
+    session = models.ForeignKey('Session', blank=True, null=True)
