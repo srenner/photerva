4c9ca7d6501811a2c7de44f0e8faf7e549910ceb
Shawn Renner <shawn.renner@gmail.com>
2016-10-18 21:14:40 -0500

    gulp task, add vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,18 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
   console.log('gulping');
+
+  gulp.src([
+    'node_modules/vue/dist/vue.js',
+    'node_modules/vue/dist/vue.min.js',
+    'node_modules/fullcalendar/dist/fullcalendar.js',
+    'node_modules/fullcalendar/dist/fullcalendar.min.js'
+  ]).pipe(gulp.dest('./mainsite/static/mainsite/js/vendor/'));
+
+  gulp.src([
+    'node_modules/fullcalendar/dist/fullcalendar.css',
+    'node_modules/fullcalendar/dist/fullcalendar.min.css'
+  ]).pipe(gulp.dest('./mainsite/static/mainsite/css/vendor/'));
+
+  console.log('gulped');
 });
