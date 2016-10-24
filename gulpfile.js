42bc3e2174dcdde871f319f7c4b022540518a017
Shawn Renner <shawn.renner@gmail.com>
2016-10-23 20:08:29 -0500

    base html, reconfigure vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,31 @@ gulp.task('default', function() {
 
   gulp.src([
     'node_modules/vue/dist/vue.js',
-    'node_modules/vue/dist/vue.min.js',
-    'node_modules/fullcalendar/dist/fullcalendar.js',
-    'node_modules/fullcalendar/dist/fullcalendar.min.js'
-  ]).pipe(gulp.dest('./mainsite/static/mainsite/js/vendor/'));
+    'node_modules/vue/dist/vue.min.js'
+  ]).pipe(gulp.dest('./mainsite/static/mainsite/vendor/vue/'));
 
   gulp.src([
+    'node_modules/fullcalendar/dist/fullcalendar.js',
+    'node_modules/fullcalendar/dist/fullcalendar.min.js',
     'node_modules/fullcalendar/dist/fullcalendar.css',
     'node_modules/fullcalendar/dist/fullcalendar.min.css'
-  ]).pipe(gulp.dest('./mainsite/static/mainsite/css/vendor/'));
+  ]).pipe(gulp.dest('./mainsite/static/mainsite/vendor/fullcalendar/'));
+
+  gulp.src([
+    'node_modules/bootstrap/dist/css/*',
+  ]).pipe(gulp.dest('./mainsite/static/mainsite/vendor/bootstrap/css'));
+
+  gulp.src([
+    'node_modules/bootstrap/dist/fonts/*',
+  ]).pipe(gulp.dest('./mainsite/static/mainsite/vendor/bootstrap/fonts'));
+
+  gulp.src([
+    'node_modules/bootstrap/dist/js/*',
+  ]).pipe(gulp.dest('./mainsite/static/mainsite/vendor/bootstrap/js'));
+
+  gulp.src([
+    'node_modules/jquery/dist/*'
+  ]).pipe(gulp.dest('./mainsite/static/mainsite/vendor/jquery/'));
 
   console.log('gulped');
 });
